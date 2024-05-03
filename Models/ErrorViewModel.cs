7949e08d3b8d793b2030b6fabaf1d1c057ab411e
Tess-mltx <maloteaux.stacy@gmail.com>
2024-05-03 15:26:03 +0200

    Import project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyKitchenApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
